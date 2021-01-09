b23dc67e4772463e9d82d3eb4fbaf53f71f3b101
Conor McFerren <cnmcferren@gmail.com>
2021-01-09 22:06:59 +0000

    Added abilities formatting

--- scripts/formatting.php
+++ scripts/formatting.php
@@ -1,6 +1,7 @@
 <?php
+    include "dbconnect.php";
 
-    function formatBlock($title, $date, $location, $description, $imageRef) {
+    function formatExperienceBlock($title, $date, $location, $description, $imageRef) {
         echo "<div class='flipcard'>";
         echo "<div class='flipcard-inner'>";
         echo "<div class='flipcard-front' style='background-image:url(" . $imageRef . ")'>";
@@ -13,8 +14,21 @@
         echo "</div>";
         echo "</div></div>";
     }
+    function formatSkillsBlock($title, $rating) {
+        echo "<td>";
+        echo "<span style='float:left;'>" . $title . "</span>";
+        $count = 5;
+        while ($count > $rating) {
+            echo "<span class='fa fa-star empty' style='float:right;'></span>";
+            $count = $count - 1;
+        }
+        while ($count > 0) {
+            echo "<span class='fa fa-star filled' style='float:right;'></span>";
+            $count = $count - 1;
+        }
+        echo "</td>";
+    }
     function printExperience() {
-        include "dbconnect.php";
         $conn = connectDatabase();
 
         $sql = "SELECT title, imageRef, timePeriod, location, description FROM experience";
@@ -22,10 +36,80 @@
 
         if ($result->num_rows> 0) {
             while ($row = mysqli_fetch_assoc($result)) {
-                formatBlock($row["title"], $row["timePeriod"], $row["location"], $row["description"], $row["imageRef"]);
+                formatExperienceBlock($row["title"], $row["timePeriod"], $row["location"], $row["description"], $row["imageRef"]);
             }
         }
 
         closeDatabase($conn);
+    }
+    function printSkills() {
+        $conn = connectDatabase();
+
+        $sql = "SELECT title, rating FROM skills";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<table>";
+            $count = 0;
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($count % 2 == 0) {
+                    echo "<tr>";
+                }
+                formatSkillsBlock($row["title"], $row["rating"]);
+                if ($count % 2 == 1) {
+                    echo "</tr>";
+                }
+
+                $count = $count + 1;
+            }
+            echo "</table>";
+        }
+
+    }
+    function printTools() {
+        $conn = connectDatabase();
+
+        $sql = "SELECT title, rating FROM tools";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<table>";
+            $count = 0;
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($count % 2 == 0) {
+                    echo "<tr>";
+                }
+                formatSkillsBlock($row["title"], $row["rating"]);
+                if ($count % 2 == 1) {
+                    echo "</tr>";
+                }
+
+                $count = $count + 1;
+            }
+            echo "</table>";
+        }
+    }
+    function printLanguages() {
+        $conn = connectDatabase();
+
+        $sql = "SELECT title, rating FROM languages";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<table>";
+            $count = 0;
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($count % 2 == 0) {
+                    echo "<tr>";
+                }
+                formatSkillsBlock($row["title"], $row["rating"]);
+                if ($count % 2 == 1) {
+                    echo "</tr>";
+                }
+
+                $count = $count + 1;
+            }
+            echo "</table>";
+        }
     }
 ?>
