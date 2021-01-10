952295541bffa5e84e9a59b3cdd04db174781edf
Conor McFerren <cnmcferren@gmail.com>
2021-01-10 17:10:23 +0000

    Created

---
+++ lockheedmartin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>W. Conor McFerren - Skunk Works</title>
+
+        <meta charset='utf-8' content="width=device-width, initial-scale=1">
+        <meta name="author" content="W. Conor McFerren">
+        <meta name="description" content="Read about my time that I spent working in Fort Worth as a Skunk Works Intern.">
+        <meta name="language" content="english"> 
+        <meta http-equiv="content-type" content="text/html">
+        <meta name="author" content="W. Conor McFerren">
+        <meta name="designer" content="W. Conor McFerren">
+        <meta name="publisher" content="W. Conor McFerren">
+        <meta name="keywords" content="Software Engineer,Ham Radio,Developer,Computer Scientist">
+        <meta property="og:title" content="Skunk Works Intern" />
+        <meta property="og:url" content="https://www.conormcferren.com/lockheedmartin.php" />
+        <meta property="og:image" content="https://www.conormcferren.com/images/f35lightning.jpg" />
+        <meta property="og:description" content="Read about my time that I spent working in Fort Worth as a Skunk Works Intern." />
+
+
+        <link rel="stylesheet" href="styles/colors.css">
+    </head>
+    <body>
+        <h1>Page Under Construction</h1>
+    </body>
+</html>
---
+++ ssrl.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>W. Conor McFerren - SSRL</title>
+
+        <meta charset='utf-8' content="width=device-width, initial-scale=1">
+        <meta name="author" content="W. Conor McFerren">
+        <meta name="description" content="Learn about my work with the UGA Small Satellite Research Laboratory.">
+        <meta name="language" content="english"> 
+        <meta http-equiv="content-type" content="text/html">
+        <meta name="author" content="W. Conor McFerren">
+        <meta name="designer" content="W. Conor McFerren">
+        <meta name="publisher" content="W. Conor McFerren">
+        <meta name="keywords" content="Software Engineer,Ham Radio,Developer,Computer Scientist">
+        <meta property="og:title" content="Small Satellite Research Laboratory" />
+        <meta property="og:url" content="https://www.conormcferren.com/ssrl.php" />
+        <meta property="og:image" content="https://www.conormcferren.com/images/spoc.JPG" />
+        <meta property="og:description" content="Learn about my work with the UGA Small Satellite Research Laboratory." />
+
+
+        <link rel="stylesheet" href="styles/colors.css">
+    </head>
+    <body>
+        <h1>Page Under Construction</h1>
+    </body>
+</html>
