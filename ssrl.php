3048e4422ce1e6104da84738f93d609c58bf4037
Conor McFerren <cnmcferren@gmail.com>
2021-01-10 21:43:40 +0000

    Added contact me

--- ssrl.php
+++ ssrl.php
@@ -18,10 +18,31 @@
         <meta property="og:image" content="https://www.conormcferren.com/images/experiences/spoc.JPG" />
         <meta property="og:description" content="Learn about my work with the UGA Small Satellite Research Laboratory." />
 
-
+        <link rel="stylesheet" href="styles/contactme.css">
         <link rel="stylesheet" href="styles/colors.css">
+        <link rel="stylesheet" href="styles/experience/landing.css">
     </head>
     <body>
-        <h1>Page Under Construction</h1>
+        <?php
+            include "scripts/experiencePages.php";
+        ?>
+        <div class="background">
+            Hello
+        </div>
+        <div class="contactme" id="contactme">
+            <div class="contactmeTitle">Contact Information</div>
+            <hr>
+            <div class="contactInfo">
+                <ul>
+                    <li><a href="https://linkedin.com/in/w-conor-mcferren"><img src="images/contactme/linkedin.svg" alt="LinkedIn"> LinkedIn</a></li>
+                    <li><a href="https://instagram.com/cnmcferren"><img src="images/contactme/instagram.svg" alt="Instagram"> Instagram</a></li>
+                    <li><a href="https://www.qrz.com/db/kn4sit"><img src="images/contactme/radio.svg" alt="QRZ"> QRZ</a></li>
+                    <li><a href="mailto:cnmcferren@gmail.com"><img src="images/contactme/email.svg" alt="Email"> Email</a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                Copyright &copy 2021 W. Conor McFerren. All Rights Reserved.
+            </div>
+        </div>
     </body>
 </html>
