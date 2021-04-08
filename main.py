c4c70b486e2fa82ee463023f117baf029a5a1071
W. Conor McFerren <cnmcferren@lightning.local>
2021-04-08 09:35:41 -0400

    Added error logging

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, send_from_directory, redirect
 import databaseconnector
 from experience import Experience
 from waitress import serve
+import sys
 
 app = Flask(__name__, 
     static_url_path='', 
@@ -14,22 +15,25 @@ app = Flask(__name__,
 @app.route('/index')
 @app.route('/home')
 def index():
-    skills = dict(databaseconnector.query("SELECT title, rating FROM skills"))
-    tools = dict(databaseconnector.query("SELECT title, rating FROM tools"))
-    languages = dict(databaseconnector.query("SELECT title, rating FROM languages"))
-    experience = databaseconnector.query("SELECT title, dates, shortDescription, longDescription, image, id FROM experience")
-    experienceStructs = []
-    for line in experience:
-        newStruct = Experience(line[0],
-                                line[1],
-                                line[2],
-                                line[3],
-                                line[4],
-                                'a' + str(line[5])
-                                )
-        experienceStructs.append(newStruct)
+    try:
+        skills = dict(databaseconnector.query("SELECT title, rating FROM skills"))
+        tools = dict(databaseconnector.query("SELECT title, rating FROM tools"))
+        languages = dict(databaseconnector.query("SELECT title, rating FROM languages"))
+        experience = databaseconnector.query("SELECT title, dates, shortDescription, longDescription, image, id FROM experience")
+        experienceStructs = []
+        for line in experience:
+            newStruct = Experience(line[0],
+                                    line[1],
+                                    line[2],
+                                    line[3],
+                                    line[4],
+                                    'a' + str(line[5])
+                                    )
+            experienceStructs.append(newStruct)
 
-    return render_template('index.html', skills=skills, tools=tools, languages=languages, experiences=experienceStructs)
+        return render_template('index.html', skills=skills, tools=tools, languages=languages, experiences=experienceStructs)
+    except Exception as e:
+        print(e, file=sys.stderr)
 
 @app.errorhandler(404)
 def page_not_found(error):
